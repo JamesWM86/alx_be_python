80ca60ddf909b32c08fdaedfd1ac54ae2e8434bd
James Mureithi <jwmjobs86@gmail.com>
2025-08-10 15:35:37 +0300

    Implement Book class with __init__, __del__, __str__, and __repr__ magic methods

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,27 +1,16 @@
 # book_class.py
 
 class Book:
-    """
-    Simple Book class demonstrating Python magic methods:
-    - __init__: constructor
-    - __del__: destructor (prints when object is deleted)
-    - __str__: readable string for users
-    - __repr__: official string representation that can recreate the object
-    """
-
-    def __init__(self, title: str, author: str, year: int):
+    def __init__(self, title, author, year):
         self.title = title
         self.author = author
         self.year = year
 
     def __del__(self):
-        # Called when the object is garbage-collected; print title as required
         print(f"Deleting {self.title}")
 
-    def __str__(self) -> str:
-        # Friendly string representation
+    def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
 
-    def __repr__(self) -> str:
-        # Official representation that can be used to recreate the object
+    def __repr__(self):
         return f"Book('{self.title}', '{self.author}', {self.year})"
